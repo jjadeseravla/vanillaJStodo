d8378530f2859778346c6d66785e5c42f44a425d
jjadeseravla <jadealvares@hotmail.co.uk>
2019-06-19 15:37:59 +0100

    added dustbin svg

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,9 +1,9 @@
 document.getElementById('add').addEventListener('click', function() {
   var value = document.getElementById('item').value; //get user input
-  // if (value) {
-  //   ;
-  // }
-  console.log(value);
+  if (value) {
+
+  }
+  //console.log(value);
 });
 
 //what kind of event and a callback are the arguments that
