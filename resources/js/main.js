4dc09c415a19a691e021ffa26a9271a4b1b53f63
jjadeseravla <jadealvares@hotmail.co.uk>
2019-06-19 15:25:47 +0100

    can log input value

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+document.getElementById('add').addEventListener('click', function() {
+  var value = document.getElementById('item').value; //get user input
+  // if (value) {
+  //   ;
+  // }
+  console.log(value);
+});
+
+//what kind of event and a callback are the arguments that
+//addEventListener takes which is the same as .onclick
